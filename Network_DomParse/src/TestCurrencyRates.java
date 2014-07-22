3791fcd0c3b5cf0248cec1f52240034f21e8dfad
KürşadOffice <Krad@10.206.92.48>
2014-07-22 15:26:43 +0300

    Dom parse ve connection sınıfı

--- Network_DomParse/src/TestCurrencyRates.java
+++ Network_DomParse/src/TestCurrencyRates.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -38,19 +43,35 @@ public class TestCurrencyRates {
 						Element firstCube =(Element) document.getElementsByTagName("Cube").item(0);
 						Element secondCube =(Element) firstCube.getElementsByTagName("Cube").item(0);
 						NodeList currencyRateNodeList = secondCube.getElementsByTagName("Cube");
+						
+						File dosya = new File("C:\\Users\\Kürþad\\Desktop\\text.txt");
+
+						Calendar cal = Calendar.getInstance();
+				    	cal.getTime();
+				    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+				    	String time = sdf.format(cal.getTime());
+				    	
+				    	
+				    	ArrayList<String> dizi_total = new ArrayList<String>();
+						dizi_total.add("DOSYANIN ÇEKÝM TARÝHÝ : " +time);
 																		
 						for (int i = 0; i < currencyRateNodeList.getLength(); i++) {
 							
 							Element currencyElement = (Element) currencyRateNodeList.item(i);
 							
-							CurrencyMainClass c = new CurrencyMainClass();
-							c.setCurrency(currencyElement.getAttribute("currency"));
-							c.setRate(currencyElement.getAttribute("rate"));
+							String currencyString =currencyElement.getAttribute("currency");
+							String rateString =currencyElement.getAttribute("rate");
 							
-							System.out.println(c.getCurrency() +" / EUR :  " + c.getRate());
+							dizi_total.add(currencyString+" / EUR :  "+ rateString);
+													
+							String line  = currencyString +" / EUR :  " +rateString;
+							
+							System.out.println(line);
+							
+							FileUtils.writeLines(dosya,dizi_total);
+							
 							}
-						
-					}
+						}
 			    
 			 		if (urlConnection !=null) {
 			 			
